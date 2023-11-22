a226ad333e8d308573e92d327051b2bd79753b4c
JacquesX <jacques.xuereb.23@um.edu.mt>
2023-11-22 15:03:09 +0100

    added map markers

--- map_widget.js
+++ map_widget.js
@@ -3,20 +3,30 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 20, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var address = "Malta"
-addresses.forEach(function(address) {
-    var username = 'JacquesX';
-    var url = `https://api.geonames.org/searchJSON?q=${encodeURIComponent(address)}&maxRows=1&username=${username}`;
-    var proxyUrl = `https://cors-anywhere.herokuapp.com/${url}`;
-            
-    fetch(proxyUrl).then(response => response.json()).then(data => {
-        var geoname = data.geonames[0];
-        if (geoname) {
-            var lat = geoname.lat;
-            var lng = geoname.lng;
-            L.marker([lat, lng]).addTo(map).bindPopup(address).openPopup();
+var username = 'JacquesX'; // Replace with your GeoNames username
+var latitude = 35.8291;
+var longitude = 14.4786;
+var radius = 20; // Radius in kilometers
+
+// Make a GET request to GeoNames API
+fetch(`https://secure.geonames.org/findNearbyJSON?lat=${latitude}&lng=${longitude}&radius=${radius}&username=${username}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok.');
+        }
+        return response.json();
+    })
+    .then(data => {
+        // Process the JSON response and add markers to the map
+        if (data && data.geonames && data.geonames.length > 0) {
+            data.geonames.forEach(place => {
+                var marker = L.marker([place.lat, place.lng]).addTo(map);
+                marker.bindPopup(`<b>${place.name}</b><br>${place.countryName}`);
+            });
+        } else {
+            console.error('No nearby places found or invalid data');
         }
     })
-    
-    .catch(error => { console.log('Error:', error);  });
-});
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
